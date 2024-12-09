2931815944b4132a6414cdb6c185a7ec2ccad5d9
大和 遠藤 <230254@jc-21.jp>
2024-12-09 17:20:08 +0900

    モデルの回転をさせたい

---
+++ ImprovedOperator.h
@@ -0,0 +1,82 @@
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+inline XMFLOAT3 operator +(const XMVECTOR& a, const XMFLOAT3& b) {
+	XMVECTOR c = XMLoadFloat3(&b);
+	XMFLOAT3 temp;
+	XMStoreFloat3(&temp, a + c);
+	return temp;
+}
+
+inline XMFLOAT3 operator +(const XMFLOAT3& a, const XMVECTOR& b) {
+	
+	XMVECTOR c = XMLoadFloat3(&a);
+	XMFLOAT3 temp;
+	XMStoreFloat3(&temp, c + b);
+	return temp;
+}
+
+inline void operator +=(const XMVECTOR& a, XMFLOAT3& b) {
+	XMVECTOR c = XMLoadFloat3(&b);
+	XMFLOAT3 temp;
+	c = a + c;
+	XMStoreFloat3(&temp, c);
+	b = temp;
+}
+
+inline void operator +=(XMFLOAT3& a, const XMVECTOR& b) {
+
+	XMVECTOR c = XMLoadFloat3(&a);
+	XMFLOAT3 temp;
+	c = c + b;
+	XMStoreFloat3(&temp, c);
+	a = temp;
+}
+
+inline XMFLOAT3 operator -(const XMVECTOR& a, const XMFLOAT3& b) {
+	XMVECTOR c = XMLoadFloat3(&b);
+	XMFLOAT3 temp;
+	XMStoreFloat3(&temp, a - c);
+	return temp;
+}
+
+inline XMFLOAT3 operator -(const XMFLOAT3& a, const XMVECTOR& b) {
+
+	XMVECTOR c = XMLoadFloat3(&a);
+	XMFLOAT3 temp;
+	XMStoreFloat3(&temp, c - b);
+	return temp;
+}
+
+inline void operator -=(const XMVECTOR& a, XMFLOAT3& b) {
+	XMVECTOR c = XMLoadFloat3(&b);
+	XMFLOAT3 temp;
+	c = a - c;
+	XMStoreFloat3(&temp, c);
+	b = temp;
+}
+
+inline void operator -=(XMFLOAT3& a, const XMVECTOR& b) {
+
+	XMVECTOR c = XMLoadFloat3(&a);
+	XMFLOAT3 temp;
+	c = c - b;
+	XMStoreFloat3(&temp, c);
+	a = temp;
+}
+
+inline XMFLOAT3 operator *(const XMVECTOR& a, const XMFLOAT3& b) {
+	XMVECTOR c = XMLoadFloat3(&b);
+	XMFLOAT3 temp;
+	XMStoreFloat3(&temp, a * c);
+	return temp;
+}
+
+inline XMFLOAT3 operator *(const XMFLOAT3& a, const XMVECTOR& b) {
+
+	XMVECTOR c = XMLoadFloat3(&a);
+	XMFLOAT3 temp;
+	XMStoreFloat3(&temp, c * b);
+	return temp;
+}
