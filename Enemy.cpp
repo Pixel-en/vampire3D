6595216fd6b3b177276704691f2bbb217e496800
enyama0526@gmail.com <enyama0526@gmail.com>
2024-12-25 00:31:54 +0900

    いったん上げ

--- Enemy.cpp
+++ Enemy.cpp
@@ -42,17 +42,20 @@ void Enemy::Update()
 	eRotFront = XMVector3Normalize(eRotFront);
 	epVec = XMVector3Normalize(epVec);
 
-	float dot = XMVectorGetX(XMVector3Dot(epVec, eRotFront));	//内積
-	float angle = acos(dot);	//ラジアン角
-	Debug::Log(angle, true);
+	float angle = XMVectorGetX(XMVector3AngleBetweenNormals(eRotFront, epVec));
+	Debug::Log(XMConvertToDegrees( angle), true);
+	transform_.rotate_.y += XMConvertToDegrees(angle);
+	//float dot = XMVectorGetX(XMVector3Dot(epVec, eRotFront));	//内積
+	//float angle = acos(dot);	//ラジアン角
+	//Debug::Log(angle, true);
 	//XMMATRIX mat = XMMatrixRotationY(angle);
 	//XMVECTOR rotVec = XMVector3TransformCoord(XMVectorSet(0, 0, 0.2f, 0), mat);
 	//rotVec = XMVector3Normalize(rotVec);	//回転ベクトル
-	float epX = XMVectorGetX(epVec);
-	if ((epX >= 0.0) - (epX < 0.0) == 1)
-		transform_.rotate_.y += XMConvertToDegrees(angle);
-	else
-		transform_.rotate_.y += -XMConvertToDegrees(angle);
+	//float epX = XMVectorGetX(epVec);
+	//if ((epX >= 0.0) - (epX < 0.0) == 1)
+	//	transform_.rotate_.y += XMConvertToDegrees(angle);
+	//else
+	//	transform_.rotate_.y += -XMConvertToDegrees(angle);
 
 	//transform_.position_ += epVec * 50.0f * Time::DeltaTime();
 }
