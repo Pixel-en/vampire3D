76fa4bcac9c1115309e3dc8a93792a2d2c1ab31c
230254 <230254@E0404-06>
2025-05-22 18:34:52 +0900

    ４次元配列を作るよ

--- Enemy.h
+++ Enemy.h
@@ -4,6 +4,37 @@
 //オブジェクトコピー用
 class Enemy :public GameObject
 {
+	enum ENEMYTYPE
+	{
+		BLUE,
+		YELLOW,
+		GREEN,
+		RED,
+		MAX
+	};
+	enum HP {
+		FULL,
+		HALF,
+		MINI,
+		MAX
+	};
+	enum ANIMATION
+	{
+		MOVE,
+		DAMEGE,
+		DEATH,
+		MAX
+	};
+	enum LOD
+	{
+		HIGH,
+		MIDDLE,
+		LOW,
+		MAX
+	};
+	int hModel_[ENEMYTYPE::MAX][HP::MAX][ANIMATION::MAX][LOD::MAX];
+	
+
 public:
 	Enemy(GameObject* parent);
 
@@ -20,5 +51,11 @@ public:
 
 	//開放
 	void Release() override;
+
+	/// <summary>
+	/// 与えたダメージ
+	/// </summary>
+	/// <returns>ダメージ</returns>
+	int CausedDamege();
 };
 
